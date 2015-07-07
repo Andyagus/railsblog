7db7d1ef871b87c209d4ed41ad1960082e863464
Andy Agus <Redagus@gmail.com>
2015-07-07 18:42:01 -0400

    Added pages for post creation and viewing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,19 @@
 class PostsController < ApplicationController
+
+   def index
+      @posts = Post.all
+   end   
+
+   def new
+      @post = Post.new
+   end
+
+   def create
+      
+      @user = current_user
+      @post = current_user.posts.create params[:post]
+      flash[:alert] = "Just created your post!"
+      redirect_to root_path
+   end
+
 end
