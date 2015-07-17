283a4e736044681f4993c0b3e75848f24b0d09e3
Andy Agus <Redagus@gmail.com>
2015-07-17 13:19:01 -0400

    fixed sign up, and edit styles and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,19 +12,11 @@ class UsersController < ApplicationController
 
    def create
          @user = User.new
-
-         if User.find_by(params[:user])
-           flash[:alert] = "This user exists already"
-           redirect_to new_user_path
-         elsif
            params[:user][:password] == params[:password_confirmation]
            @user = User.create params[:user]
            flash[:alert] = "#{@user.username} has been signed up"
            redirect_to users_path
-         else
-           flash[:alert] = "Your password did not match confirmation"
-           redirect_to new_user_path
-         end
+        
    end
 
    def show
